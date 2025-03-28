7ca89959bc5cf1d3405286093f120152d1159182
dohehehe <doheerlob@gmail.com>
2025-03-28 16:33:31 +0900

    💄 배경 지도 스타일 변경, 지도 로딩 애니메이션

--- src/components/Map3D.jsx
+++ src/components/Map3D.jsx
@@ -14,7 +14,7 @@ const Map3D = () => {
 
       Module.postRun = function () {
         Module.initialize({
-          container: document.getElementById('map'),
+          container: document.getElementById('map3D'),
           terrain: {
             dem: {
               url: 'https://xdworld.vworld.kr',
@@ -38,16 +38,18 @@ const Map3D = () => {
         });
 
         Module.getViewCamera().setLocation(
-          new Module.JSVector3D(126.9958537267327, 37.56628736129694, 500)
+          new Module.JSVector3D(126.9958537267327, 37.56628736129694, 40.496476641856134)
         );
         Module.getViewCamera().setDirect(24);
         Module.getViewCamera().setFov(45);
+        Module.getViewCamera().setTilt(10);
 
-        setTimeout(() =>
-          Module.getViewCamera().moveOval(new Module.JSVector3D(126.9958537267327, 37.56628736129694, 40.496476641856134), 10, 24, 1),
-          // Module.getViewCamera().setTilt(10), 
-          3000);
-
+        // 구글 배경지도가 로딩속도가 빠름
+        // 구글은 푸른색감 기본지도는 초록색감 -> 최종 디자인보고 결정
+        var google = Module.GoogleMap();
+        google.layername = "satellite";
+        google.quality = "middle";
+        google.zerolevelOffset = 1;
 
         Module.getTileLayerList().createXDServerLayer({
           url: 'https://xdworld.vworld.kr',
@@ -64,14 +66,13 @@ const Map3D = () => {
 
     document.body.appendChild(script);
 
-    // Cleanup the script on component unmount
     return () => {
       document.body.removeChild(script);
     };
   }, []);
 
   return (
-    <div id="map" style={{ width: '100%', height: '100vh' }}></div>
+    <div id="map3D" style={{ width: '100dvw', height: '100dvh' }}></div>
   );
 };
 
