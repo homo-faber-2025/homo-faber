ce4c77453c8d5ee80c0fa9042b0283f933f853e1
dohehehe <doheerlob@gmail.com>
2025-09-05 22:30:40 +0900

    🚀 Image 에 supabse 이미지 허용

--- next.config.mjs
+++ next.config.mjs
@@ -3,5 +3,15 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'ugybuzfzmywmnbsikhtf.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 export default nextConfig;
